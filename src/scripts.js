eb023f9923c4b87cea3449ea1563f0316dadffb7
“Lauren <lucerolaurenm@gmail.com>
2020-03-30 17:08:10 -0600

    Write function to display all recipes

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,24 @@
-console.log('Hello world');
+window.addEventListener('load', function() {
+  displayAllRecipes();
+});
+
+function displayAllRecipes() {
+  let mainPage = document.querySelector('.main-page');
+  recipeData.forEach(recipe => {
+    let newRecipe = new Recipe(recipe);
+    mainPage.innerHTML += `<section class="recipe-card">
+      <img class="recipe-img" src=${newRecipe.image} alt=${newRecipe.name}>
+      <div class="name-n-btns">
+      <h4 class="recipe-name">${newRecipe.name}</h4>
+      <i class="far fa-heart"></i>
+      <i class="far fa-bookmark"></i>
+      <div>
+    </section>`
+ });
+}
+
+// display user data on home page
+// assign random user based on index
+// let chosen user be new instance of user with random user
+
+// user to add/remove favorites
